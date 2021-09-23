a9492c0547ba7ba664192f22c7adf13558a9873c
Damniel009 <vargadaniel28@users.noreply.github.com>
2021-09-23 21:47:25 +0300

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,15 @@ module.exports = {
       },
       __key: "images",
     },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
   ],
 };
