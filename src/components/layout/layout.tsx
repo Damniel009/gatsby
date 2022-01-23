91cfce8645f20be0025d96ff86f0a1cf97b0c1ed
Damniel009 <vargadaniel28@users.noreply.github.com>
2022-01-23 12:10:23 +0200

    Started working on main page+carousel

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,12 +2,17 @@ import * as React from "react";
 
 import Footer from "./footer";
 import Navbar from "./navbar";
+import { Helmet } from "react-helmet";
 
 import "./Searchbar.scss";
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <div>
+      <Helmet>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </Helmet>
+
       <Navbar></Navbar>
 
       <main>{children}</main>
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,53 +1,39 @@
 import * as React from "react";
 import { Link } from "gatsby";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
-
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <div lang="en">
+      <div>
+        <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+          <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+            <div className="relative">
+              <div className="absolute">
+                <div className="">
+                  <h1 className="my-2 text-gray-800 font-bold text-2xl">
+                    Looks like you've found the doorway to the great nothing
+                  </h1>
+                  <p className="my-2 text-gray-800">
+                    Sorry about that! Please visit our hompage to get where you
+                    need to go.
+                  </p>
+                  <button className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
+                    Take me there!
+                  </button>
+                </div>
+              </div>
+              <div>
+                <img src="https://i.ibb.co/G9DC8S0/404-2.png" />
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src="https://i.ibb.co/ck1SGFJ/Group.png" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
