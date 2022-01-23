ce8174804d7f01a43a2abe1c0616ad0a249d0105
Damniel009 <vargadaniel28@users.noreply.github.com>
2022-01-23 13:22:49 +0200

    Worked on carousel

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -14,7 +14,7 @@ import {
 } from "pure-react-carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
 import CustomSlide from "./slide";
-
+import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
 import "./style.scss";
 
 function eventLogger(ev) {
@@ -25,66 +25,59 @@ function eventLogger(ev) {
 export default function Carousel() {
   const [slide, setSlide] = useState(0);
   return (
-    <div className="mockBackground">
+    <div className="container h-25">
       <CarouselProvider
         visibleSlides={1}
-        totalSlides={1}
-        naturalSlideWidth={100}
-        naturalSlideHeight={100}
+        totalSlides={4}
+        naturalSlideWidth={200}
+        naturalSlideHeight={70}
         isIntrinsicHeight={true}
+        infinite={true}
+        isPlaying={true}
       >
-        <Slider className="mockSlider">
-          <Slide className="mockSlide" index={0}>
-            First slide
-          </Slide>
-        </Slider>
-        <ButtonFirst>First</ButtonFirst>
-        <ButtonBack>Back</ButtonBack>
-        <ButtonNext>Next</ButtonNext>
-        <ButtonLast>Last</ButtonLast>
-        <DotGroup dotNumbers />
+        <div className="d-flex">
+          <ButtonBack>
+            <ArrowBackIcon />
+          </ButtonBack>
+          <Slider className="mockSlider">
+            <Slide className="mockSlide" index={0}>
+              <h4>Maximális komfortérzet, minimális energiafogyasztás!</h4>
+              <div>
+                Padló- és mennyezetfűtési / -hűtési megoldások, professzionális
+                kivitelezésben, garanciával.
+              </div>
+            </Slide>
+            <Slide className="mockSlide" index={1}>
+              <h4>Tudta, hogy rezsiköltsége lehet akár 0 Ft is?</h4>
+              <div>
+                Ha szeretné csökkenteni fűtésszámláját, hívja a
+                Hőszivattyú-Gépész Kft.-t!
+              </div>
+            </Slide>
+            <Slide className="mockSlide" index={2}>
+              <h4>Hővisszanyerős szellőzés: megoldás allergia ellen!</h4>
+              <div>
+                Teljeskörű beltéri megoldások a tisztított, pollenmentes levegő
+                érdekében!
+              </div>
+            </Slide>
+            <Slide className="mockSlide" index={0}>
+              <h4>Környezetbarát hőenergia az Ön otthonában is!</h4>
+              <div>
+                Minőségi megoldásokat kínálunk hosszú távra, több éves szakmai
+                tapasztalattal.
+              </div>
+            </Slide>
+          </Slider>
+          {/* <ButtonFirst>First</ButtonFirst> */}
+          <ButtonNext>
+            <ArrowForwardIcon />
+          </ButtonNext>
+        </div>
+        {/* <ButtonLast>Last</ButtonLast> */}
+        <DotGroup dotNumbers disableActiveDots />
       </CarouselProvider>
     </div>
-
-    // <CarouselProvider
-    //   visibleSlides={1}
-    //   totalSlides={6}
-    //   naturalSlideWidth={400}
-    //   naturalSlideHeight={500}
-    // >
-    //   <h2 className={s.headline}>
-    //     Horizontal Carousel (With Master Loading Spinner)
-    //   </h2>
-    //   <p>
-    //     Horizontal Carousel with regular, non-zooming images and dotNumbers on
-    //     the Dot Group.
-    //   </p>
-    //   <Slider className={s.slider}>
-    //     <Slide tag="a" index={0}>
-    //       <Image src="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80" />
-    //     </Slide>
-    //     <Slide tag="a" index={1}>
-    //       <Image src="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80" />
-    //     </Slide>
-    //     <Slide tag="a" index={2}>
-    //       <Image src="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80" />
-    //     </Slide>
-    //     <Slide tag="a" index={3}>
-    //       <Image src="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80" />
-    //     </Slide>
-    //     <Slide tag="a" index={4}>
-    //       <Image src="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80" />
-    //     </Slide>
-    //     <Slide tag="a" index={5}>
-    //       <Image src="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80" />
-    //     </Slide>
-    //   </Slider>
-    //   <ButtonFirst>First</ButtonFirst>
-    //   <ButtonBack>Back</ButtonBack>
-    //   <ButtonNext>Next</ButtonNext>
-    //   <ButtonLast>Last</ButtonLast>
-    //   <DotGroup dotNumbers />
-    // </CarouselProvider>
 
     // <React.Fragment>
     //   <div>
