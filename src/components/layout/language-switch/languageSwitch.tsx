22d8595723ba888b52dc7f81d05184f09448806d
Damniel009 <vargadaniel28@users.noreply.github.com>
2022-01-30 17:22:53 +0200

    Started working on language switch

--- src/components/layout/language-switch/languageSwitch.tsx
+++ src/components/layout/language-switch/languageSwitch.tsx
@@ -1,22 +1,51 @@
 import React from "react";
 import { Link, useI18next } from "gatsby-plugin-react-i18next";
-import { Select } from "@chakra-ui/react";
-import { Radio, RadioGroup, Stack } from "@chakra-ui/react";
+
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverBody,
+  PopoverArrow,
+  Radio,
+  RadioGroup,
+  VStack,
+  Button,
+} from "@chakra-ui/react";
 
 const LanguageSwitch = () => {
   const { languages, originalPath, language } = useI18next();
 
   const [value, setValue] = React.useState(language);
   return (
-    <RadioGroup onChange={setValue} value={value}>
-      <Stack direction="row">
-        {languages.map((lng) => (
-          <Link to={originalPath} language={lng} key={lng}>
-            <Radio value={lng}>{lng}</Radio>
-          </Link>
-        ))}
-      </Stack>
-    </RadioGroup>
+    <Popover>
+      <PopoverTrigger>
+        {/* <img src="src/images/icons/hu.svg"></img> */}
+        <Button
+          leftIcon={
+            <i className={language === "hu" ? "hu-flag" : "ro-flag"}></i>
+          }
+        >
+          {language === "hu" ? "HU" : "RO"}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverArrow />
+        <PopoverBody>
+          <RadioGroup onChange={setValue} value={value}>
+            <VStack direction="row">
+              {languages.map((lng) => (
+                <Link to={originalPath} language={lng} key={lng}>
+                  <Radio value={lng}>
+                    {lng === "hu" ? "Magyar" : "Romana"}
+                  </Radio>
+                </Link>
+              ))}
+            </VStack>
+          </RadioGroup>
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
   );
 };
 
